6d8668c7a60b22a2a309ce854662797cba9a14b3
Chris Williamson <chriswill0629@gmail.com>
2023-05-24 08:51:53 -0400

    adjusting http

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 	"net/http"
 )
@@ -10,6 +11,7 @@ const portNumber = ":8080"
 // Home is the home page handler
 func Home(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "This is the homepage")
+
 }
 
 // About is the about page handler
@@ -23,10 +25,30 @@ func addvalues(x, y int) int {
 	return x + y
 }
 
+func Divide(w http.ResponseWriter, r *http.Request) {
+	f, err := divideValues(100.0, 0.0)
+	if err != nil {
+		fmt.Fprintf(w, "Cannot divide by 0")
+		return
+	}
+
+	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", 100.0, 0.0, f))
+}
+
+func divideValues(x, y float32) (float32, error) {
+	if y <= 0 {
+		err := errors.New("cannot divide by zero")
+		return 0, err
+	}
+	result := x / y
+	return result, nil
+}
+
 // main is the server handler
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
+	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting Server on port%s", portNumber))
 
