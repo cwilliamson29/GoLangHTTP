0d8c13c2cd4d353656ea19df0584f25db9d425f0
Chris Williamson <chriswill0629@gmail.com>
2023-05-24 20:28:57 -0400

    adjusting for web server

--- main.go
+++ main.go
@@ -1,54 +1,16 @@
 package main
 
 import (
-	"errors"
 	"fmt"
 	"net/http"
 )
 
 const portNumber = ":8080"
 
-// Home is the home page handler
-func Home(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "This is the homepage")
-
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	sum := addvalues(2, 2)
-	_, _ = fmt.Fprintf(w, fmt.Sprintf("This is about apge and value: %d", sum))
-}
-
-// addvalues is adding 2 integers and returns sum
-func addvalues(x, y int) int {
-	return x + y
-}
-
-func Divide(w http.ResponseWriter, r *http.Request) {
-	f, err := divideValues(100.0, 0.0)
-	if err != nil {
-		fmt.Fprintf(w, "Cannot divide by 0")
-		return
-	}
-
-	fmt.Fprintf(w, fmt.Sprintf("%f divide by %f is %f", 100.0, 0.0, f))
-}
-
-func divideValues(x, y float32) (float32, error) {
-	if y <= 0 {
-		err := errors.New("cannot divide by zero")
-		return 0, err
-	}
-	result := x / y
-	return result, nil
-}
-
 // main is the server handler
 func main() {
 	http.HandleFunc("/", Home)
 	http.HandleFunc("/about", About)
-	http.HandleFunc("/divide", Divide)
 
 	fmt.Println(fmt.Sprintf("Starting Server on port%s", portNumber))
 
